659dd82c636bebb51cba6264e23337d1d6c4b43a
Muhammad Hasan <muhammad.hasan@pinhome.id>
2021-01-14 17:42:54 +0700

    test: add test update user

--- test/user_test.go
+++ test/user_test.go
@@ -20,27 +20,68 @@ func TestGetUser(t *testing.T) {
 	testCase := make([]interfaces.TestStruct, 0)
 	// A correct test case
 	testCase = append(testCase, interfaces.TestStruct{
-		Input:              "mhasan01",
+		Username:           "mhasan01",
 		ExpectedStatusCode: 200,
 	})
 	// A false test case
 	testCase = append(testCase, interfaces.TestStruct{
-		Input:              "NotMhasan01",
+		Username:           "NotMhasan01",
 		ExpectedStatusCode: 400,
 	})
 
 	// Check every testcase
 	for _, tc := range testCase {
-		fmt.Println(tc.Input)
+		fmt.Println(tc.Username)
 		// Make a request to the /user/{username}
-		req, err := http.NewRequest("GET", fmt.Sprintf("/user/%v", tc.Input), bytes.NewBufferString(tc.Input))
+		req, err := http.NewRequest("GET", fmt.Sprintf("/user/%v", tc.Username), bytes.NewBufferString(""))
 		if err != nil {
-			t.Errorf("Error trying to get new request get to /user/%v: %v", tc.Input, err)
+			t.Errorf("Error trying to get new request get to /user/%v: %v", tc.Username, err)
 		}
-		req = mux.SetURLVars(req, map[string]string{"username": tc.Input})
+		// Sets the path username
+		req = mux.SetURLVars(req, map[string]string{"username": tc.Username})
 		// Create http test
 		r := httptest.NewRecorder()
 		h := http.HandlerFunc(user.GetUserHandler)
+		h.ServeHTTP(r, req)
+		// Assert the result with the expected result
+		assert.Equal(t, r.Code, tc.ExpectedStatusCode)
+		fmt.Println()
+	}
+}
+
+// TestUpdateUser is to test updating a user by its username
+// it tests the endpoint /user/{username}
+func TestUpdateUser(t *testing.T) {
+	// Create a slice of testcase struct
+	testCase := make([]interfaces.TestStruct, 0)
+	// A correct test case
+	testCase = append(testCase, interfaces.TestStruct{
+		Username:           "tester",
+		Input:              `"password":"tester"`,
+		ExpectedStatusCode: 200,
+	})
+	// A false test case [cannot change other person user]
+	testCase = append(testCase, interfaces.TestStruct{
+		Username:           "mhasan01",
+		Input:              `"password":"tester"`,
+		ExpectedStatusCode: 400,
+	})
+
+	// Check every testcase
+	for _, tc := range testCase {
+		fmt.Println(tc.Username)
+		// Make a request to the /user/{username}
+		req, err := http.NewRequest("PUT", fmt.Sprintf("/user/%v", tc.Username), bytes.NewBufferString(tc.Input))
+		if err != nil {
+			t.Errorf("Error trying to get new request PUT to /user/%v: %v", tc.Username, err)
+		}
+		// Sets the path username
+		req = mux.SetURLVars(req, map[string]string{"username": tc.Username})
+		// Sets the token for the header
+		req.Header.Set("Token", Token)
+		// Create http test
+		r := httptest.NewRecorder()
+		h := http.HandlerFunc(user.UpdateUserHandler)
 		h.ServeHTTP(r, req)
 		// Assert the result with the expected result
 		assert.Equal(t, r.Code, tc.ExpectedStatusCode)
