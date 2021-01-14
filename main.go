6910e09e961b938b37fafdd7bdc7967fb1567cd5
Muhammad Hasan <muhammad.hasan@pinhome.id>
2021-01-14 09:22:48 +0700

    docs: add service description

--- main.go
+++ main.go
@@ -1,3 +1,12 @@
+// @Version 1.0.0
+// @Title Backend API Discussion Forum
+// @Description The Backend API which handles RESTful API that provides the backend for a discussion forum with Users, Threads and Posts
+// @ContactName Muhammad Hasan
+// @ContactEmail muhammad.hasan@pinhome.id
+// @LicenseName MIT
+// @LicenseURL https://en.wikipedia.org/wiki/MIT_License
+// @Security AuthorizationHeader read write
+// @SecurityScheme AuthorizationHeader JWT Token Authorization
 package main
 
 import (
