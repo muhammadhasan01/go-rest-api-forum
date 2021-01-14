35931a4ae07a756a121d90825251be8ee7eead07
Muhammad Hasan <muhammad.hasan@pinhome.id>
2021-01-14 17:48:07 +0700

    docs: add more info to Token

--- test/token.go
+++ test/token.go
@@ -1,4 +1,5 @@
 package test
 
 // Token is used for every test that needs it
+// this is the JWTToken for user tester
 const Token string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyMDUsInVzZXJuYW1lIjoidGVzdGVyIn0.Qf1IxctVIPxT2EL2XwoMn1FKShT0-yq3Vxj6sj8wXTI"
