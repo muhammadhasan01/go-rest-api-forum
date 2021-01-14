7c4424a0430e9c8cacfc5851c9e414e8122ee190
Muhammad Hasan <muhammad.hasan@pinhome.id>
2021-01-14 18:18:20 +0700

    test: add test delete thread

--- test/thread_test.go
+++ test/thread_test.go
@@ -117,7 +117,7 @@ func TestUpdateThread(t *testing.T) {
 
 	// Check every testcase
 	for _, tc := range testCase {
-		fmt.Println(tc.ThreadID)
+		fmt.Println(tc.ThreadID, tc.Input)
 		// Make a request to the /user/{username}
 		req, err := http.NewRequest("GET", fmt.Sprintf("/thread/%v", tc.ThreadID), bytes.NewBufferString(tc.Input))
 		if err != nil {
@@ -130,6 +130,54 @@ func TestUpdateThread(t *testing.T) {
 		// Create http test
 		r := httptest.NewRecorder()
 		h := http.HandlerFunc(thread.UpdateThreadHandler)
+		h.ServeHTTP(r, req)
+		// Assert the result with the expected result
+		assert.Equal(t, r.Code, tc.ExpectedStatusCode)
+		fmt.Println()
+	}
+}
+
+// TestDeleteThread is to test deleting a thread by a threadID
+// it tests the endpoint DELETE /thread/{threadID}
+func TestDeleteThread(t *testing.T) {
+	// Create a slice of testcase struct
+	testCase := make([]interfaces.TestStruct, 0)
+	// A correct test case
+	testCase = append(testCase, interfaces.TestStruct{
+		ThreadID:           "11",
+		ExpectedStatusCode: 200,
+	})
+	// A false test case [id not found]
+	testCase = append(testCase, interfaces.TestStruct{
+		ThreadID:           "0",
+		ExpectedStatusCode: 400,
+	})
+	// A false test case [id not an integer]
+	testCase = append(testCase, interfaces.TestStruct{
+		ThreadID:           "notAnInteger",
+		ExpectedStatusCode: 400,
+	})
+	// A false test case [cannot delete other person thread]
+	testCase = append(testCase, interfaces.TestStruct{
+		ThreadID:           "2",
+		ExpectedStatusCode: 400,
+	})
+
+	// Check every testcase
+	for _, tc := range testCase {
+		fmt.Println(tc.ThreadID)
+		// Make a request to the /user/{username}
+		req, err := http.NewRequest("DELETE", fmt.Sprintf("/thread/%v", tc.ThreadID), bytes.NewBufferString(""))
+		if err != nil {
+			t.Errorf("Error trying to get new request get to /thread/%v: %v", tc.ThreadID, err)
+		}
+		// Sets the path threadID
+		req = mux.SetURLVars(req, map[string]string{"threadID": tc.ThreadID})
+		// Sets the token for the header
+		req.Header.Set("Token", Token)
+		// Create http test
+		r := httptest.NewRecorder()
+		h := http.HandlerFunc(thread.DeleteThreadHandler)
 		h.ServeHTTP(r, req)
 		// Assert the result with the expected result
 		assert.Equal(t, r.Code, tc.ExpectedStatusCode)
