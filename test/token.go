0c7af33e20de4ae09d08e493e6989b80d4e77993
Muhammad Hasan <muhammad.hasan@pinhome.id>
2021-01-14 20:42:50 +0700

    test: update Token

--- test/token.go
+++ test/token.go
@@ -2,4 +2,4 @@ package test
 
 // Token is used for every test that needs it
 // this is the JWTToken for user tester
-const Token string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyMDksInVzZXJuYW1lIjoidGVzdGVyIn0.avdjP1H5EguuICbfDv0Bqn8IlF5xqDXjOdDqRwVVCHc"
+const Token string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyMTAsInVzZXJuYW1lIjoidGVzdGVyIn0.ALLPRLNlxf_mVTEGcWwWR-HUQ_T2pjalrGI5Nzv4Enw"
